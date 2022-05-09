1b308c149f8b27a00c52665d434ba2cd95f3648d
JoasGomes <jojoca.vitor777@gmail.com>
2022-05-08 23:49:05 -0300

    appSendMail-part4

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -1,5 +1,14 @@
 <?php
 
+    require "./bibliotecas/PHPMailer-6.0.0/src/Exception.php";
+    require "./bibliotecas/PHPMailer-6.0.0/src/OAuth.php";
+    require "./bibliotecas/PHPMailer-6.0.0/src/PHPMailer.php";
+    require "./bibliotecas/PHPMailer-6.0.0/src/POP3.php";
+    require "./bibliotecas/PHPMailer-6.0.0/src/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
     //print_r($_POST);
 
     class Mensagem {
@@ -36,10 +45,45 @@
 
     //print_r($mensagem);
 
-    if($mensagem->mensagemValida()){
-        echo 'mensagem é valida';
-    } else {
-        echo 'mensagem inválida';
+    if(!$mensagem->mensagemValida()){
+        echo 'mensagem não é valida';
+        die();
+    }
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'user@example.com';                 // SMTP username
+        $mail->Password = 'secret';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+    
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+        $mail->addAddress('ellen@example.com');               // Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Não foi possível enviar este email! Por favor,tente novamente.';
+        echo 'Detalhes do erro: ' . $mail->ErrorInfo;
     }
 
 
